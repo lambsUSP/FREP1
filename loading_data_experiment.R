548e30a026adc70a1dff4094a67d260f1f5e87c3
Lambs USP <lambs@usp.br>
2025-05-16 09:27:17 -0300

    Update loading_data_experiment.R
    Update following the addition of new experimental data.

--- loading_data_experiment.R
+++ loading_data_experiment.R
@@ -5,45 +5,45 @@ loading_data_experiment <- function() {
   
   data <- data.frame(
     # No. screened
-    data_screened_cage1           = c(100, 148, 1253, 1066, 1036, 1033, 1427, 1000),
-    data_screened_cage2           = c(100, 262, 1538, 1221, 1052, 1178, 1333, 1053),
-    data_screened_cage3           = c(100, 1406, 1061, 1118, 1434, 1080, 1027, 1032),
+    data_screened_cage1 = c(100,0148,1253,1066,1036,1033,1427,1000,1065,1175,1158),
+    data_screened_cage2 = c(100,0262,1538,1221,1052,1178,1333,1053,1137,1309,1173),
+    data_screened_cage3 = c(100,1406,1061,1118,1434,1080,1027,1032,1068,1212,1230),
     
     # phenotype ----
-    data_phenotype_V9_cage1       = c(50, 81, 258, 524, 598, 362, 545, 421),
-    data_phenotype_V9_cage2       = c(50, 167, 402, 570, 533, 505, 667, 472),
-    data_phenotype_V9_cage3       = c(50, 208, 225, 458, 359, 432, 440, 390),
+    data_phenotype_V9_cage1  = c(50,081,258,524,598,362,545,421,467,480,386),
+    data_phenotype_V9_cage2  = c(50,167,402,570,533,505,667,472,352,446,386),
+    data_phenotype_V9_cage3  = c(50,208,225,458,359,432,440,390,447,387,275),
     
     # phenotype ----
-    data_phenotype_LDonly_cage1   = c(0, 15, 147, 211, 129, 135, 257, 126),
-    data_phenotype_LDonly_cage2   = c(0, 27, 163, 183, 139, 167, 209, 137),
-    data_phenotype_LDonly_cage3   = c(0, 231, 138, 145, 323, 203, 189, 211),
+    data_phenotype_LDonly_cage1   = c(0,015,147,211,129,135,257,126,156,187,233),
+    data_phenotype_LDonly_cage2   = c(0,027,163,183,139,167,209,137,185,240,158),
+    data_phenotype_LDonly_cage3   = c(0,231,138,145,323,203,189,211,214,232,201),
     
-    data_phenotype_GFPLonly_cage1 = c(50, 47, 481, 322, 408, 388, 513, 408),
-    data_phenotype_GFPLonly_cage2 = c(50, 83, 632, 429, 405, 480, 603, 390),
-    data_phenotype_GFPLonly_cage3 = c(50, 525, 401, 492, 352, 330, 337, 339),
+    data_phenotype_GFPLonly_cage1 = c(050,047,481,322,408,388,513,408,499,438,367),
+    data_phenotype_GFPLonly_cage2 = c(050,083,632,429,405,480,603,390,491,430,475),
+    data_phenotype_GFPLonly_cage3 = c(050,525,401,492,352,330,337,339,367,391,424),
     
-    data_phenotype_LDGFPL_cage1   = c(50, 86, 625, 533, 499, 510, 657, 466),
-    data_phenotype_LDGFPL_cage2   = c(50, 152, 743, 609, 508, 531, 521, 526),
-    data_phenotype_LDGFPL_cage3   = c(50, 650, 522, 481, 759, 547, 501, 482),
+    data_phenotype_LDGFPL_cage1   = c(050,086,625,533,499,510,657,466,410,550,558),
+    data_phenotype_LDGFPL_cage2   = c(050,152,743,609,508,531,521,526,461,639,540),
+    data_phenotype_LDGFPL_cage3   = c(050,650,522,481,759,547,501,482,487,589,605),
     
     # No. genotyped ----
-    data_genotyped_cage1          = c(50, 50, 50, 50, 50, 50, 50, 50),
-    data_genotyped_cage2          = c(50, 50, 50, 50, 50, 50, 50, 50),
-    data_genotyped_cage3          = c(50, 50, 50, 50, 50, 50, 50, 50),
+    data_genotyped_cage1= rep(50,11),
+    data_genotyped_cage2= rep(50,11),
+    data_genotyped_cage3= rep(50,11),
     
     # allele frequency ----
-    data_allelefreq_L224_cage1    = 50 * c(75, 23.21, 8.93, 12.18, 11.03, 10.94, 9.73, 3.08) / 100,
-    data_allelefreq_L224_cage2    = 50 * c(75, 15.9, 28.94, 15.59, 7.13, 6.07, 4.59, 7.04) / 100,
-    data_allelefreq_L224_cage3    = 50 * c(75, 23.3, 17.84, 16.28, 7.28, 11.41, 8.96, 3.68) / 100,
+    data_allelefreq_L224_cage1    = 50 * c(75.00,23.21,08.93,12.18,11.03,10.94,09.73,03.08,07.85,08.91,12.20) / 100,
+    data_allelefreq_L224_cage2    = 50 * c(75.00,15.90,28.94,15.59,07.13,06.07,04.59,07.04,06.62,03.20,00.70) / 100,
+    data_allelefreq_L224_cage3    = 50 * c(75.00,23.30,17.84,16.28,07.28,11.41,08.96,03.68,00.68,01.34,01.63) / 100,
     
-    data_allelefreq_Q224_cage1    = 50 * c(25, 74.76, 88.09, 84.28, 87.31, 88.38, 89.11, 94.85) / 100,
-    data_allelefreq_Q224_cage2    = 50 * c(25, 74.82, 66.4, 76.73, 90.39, 93.33, 94.36, 90.85) / 100,
-    data_allelefreq_Q224_cage3    = 50 * c(25, 71.8, 77.53, 76.49, 90.19, 84.37, 89.36, 94.58) / 100,
+    data_allelefreq_Q224_cage1    = 50 * c(25.00,74.76,88.09,84.28,87.31,88.38,89.11,94.85,91.81,87.42,87.26) / 100,
+    data_allelefreq_Q224_cage2    = 50 * c(25.00,74.82,66.40,76.73,90.39,93.33,94.36,90.85,92.79,94.52,98.76) / 100,
+    data_allelefreq_Q224_cage3    = 50 * c(25.00,71.80,77.53,76.49,90.19,84.37,89.36,94.58,98.82,96.31,97.87) / 100,
     
-    data_allelefreq_NHEJ_cage1    = 50 * c(0, 2.03, 2.98, 3.54, 1.67, 0.68, 1.16, 2.07) / 100,
-    data_allelefreq_NHEJ_cage2    = 50 * c(0, 9.28, 4.66, 7.68, 2.48, 0.6, 1.05, 2.11) / 100,
-    data_allelefreq_NHEJ_cage3    = 50 * c(0, 4.9, 4.64, 7.23, 2.53, 4.22, 1.68, 1.74) / 100
+    data_allelefreq_NHEJ_cage1    = 50 * c(0.00,2.03,2.98,3.54,1.67,0.68,1.16,2.07,0.34,3.67,0.54) / 100,
+    data_allelefreq_NHEJ_cage2    = 50 * c(0.00,9.28,4.66,7.68,2.48,0.60,1.05,2.11,0.59,2.28,0.54) / 100,
+    data_allelefreq_NHEJ_cage3    = 50 * c(0.00,4.90,4.64,7.23,2.53,4.22,1.68,1.74,0.50,2.35,0.50) / 100
   )
   
   return(data)
